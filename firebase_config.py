354a513c71bf069e36eb77d7c606f0bca5ae80e9
zerofluxgiven <56246710+zerofluxgiven@users.noreply.github.com>
2025-05-18 23:12:02 -0600

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,13 +1,20 @@
+import streamlit as st
 import firebase_admin
 from firebase_admin import credentials
 
 def initialize_firebase():
     if not firebase_admin._apps:
         cred = credentials.Certificate({
-            "type": "service_account",
-            "project_id": st.secrets["firebase_project_id"],
-            "private_key": st.secrets["private_key"].replace("\\n", "\n"),
-            "client_email": st.secrets["client_email"],
-            "token_uri": "https://oauth2.googleapis.com/token"
+            "type": st.secrets["firebase_admin"]["type"],
+            "project_id": st.secrets["firebase_admin"]["project_id"],
+            "private_key_id": st.secrets["firebase_admin"]["private_key_id"],
+            "private_key": st.secrets["firebase_admin"]["private_key"].replace("\\n", "\n"),
+            "client_email": st.secrets["firebase_admin"]["client_email"],
+            "client_id": st.secrets["firebase_admin"]["client_id"],
+            "auth_uri": st.secrets["firebase_admin"]["auth_uri"],
+            "token_uri": st.secrets["firebase_admin"]["token_uri"],
+            "auth_provider_x509_cert_url": st.secrets["firebase_admin"]["auth_provider_x509_cert_url"],
+            "client_x509_cert_url": st.secrets["firebase_admin"]["client_x509_cert_url"],
+            "universe_domain": st.secrets["firebase_admin"].get("universe_domain", "googleapis.com")
         })
         firebase_admin.initialize_app(cred)
