d7487d55915abfee6918192b9a46950324b9f6d7
zerofluxgiven <56246710+zerofluxgiven@users.noreply.github.com>
2025-05-22 00:04:37 -0600

    Update admin_panel.py

--- admin_panel.py
+++ admin_panel.py
@@ -2,6 +2,11 @@ import streamlit as st
 from auth import require_role
 from admin_utilities import admin_utilities_ui
 from tag_merging import tag_merging_ui
+from roles import user_admin_ui
+import streamlit as st
+from auth import require_role
+from admin_utilities import admin_utilities_ui
+from tag_merging import tag_merging_ui
 from user_admin import user_admin_ui
 
 # ----------------------------
