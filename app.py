baf9f4332895c6b1e1b49ea22264ea111e3b2ff0
zerofluxgiven <56246710+zerofluxgiven@users.noreply.github.com>
2025-06-10 15:03:00 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Mountain Medicine Catering",
+    page_title="Mountain ⛰️ Medicine",
     layout="wide",
     initial_sidebar_state="collapsed"
 )
