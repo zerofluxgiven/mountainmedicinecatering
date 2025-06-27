3f4012dcb4d61eacb4290ce16c3bee7c0413ada6
zerofluxgiven <56246710+zerofluxgiven@users.noreply.github.com>
2025-06-27 12:22:22 -0600

    Delete profile.py

--- profile.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from auth import load_user_session, get_user_id, get_user_role
-from firebase_admin import firestore
-
-db = firestore.client()
-COLLECTION_USERS = "users"
-COLLECTION_EVENTS = "events"
-COLLECTION_RECIPES = "recipes"
